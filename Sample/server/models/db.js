9dda2253dcc5e0d3e3a34383da535e0d853ffa0e
yamunanga <77024699+yamunanga@users.noreply.github.com>
2021-04-27 21:12:43 +0530

    added heroku

--- Sample/server/models/db.js
+++ Sample/server/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //use find and modify add
-mongoose.connect(process.env.MONGODB_URI,{ useFindAndModify: false }, (err) => {
+mongoose.connect(process.env.MONGODB_URI,{ useFindAndModify: false,useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
